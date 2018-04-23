efc3c25e88450c8d99b0e7779aab6828e2f8b298
Jaime Deverall <jaime1deverall@gmail.com>
2018-04-23 14:57:09 -0700

    using zipcode csv instead of geocoder API

--- exploration.py
+++ exploration.py
@@ -1,33 +1,58 @@
-#https://twitter.com/search?l=&q=%23excercise%20near%3A%22Stanford%2C%20CA%22%20within%3A15mi&src=typd
-
-#https://api.twitter.com/1.1/search/tweets.json?l=&q=%23excercise%20near%3A%22Stanford%2C%20CA%22%20within%3A15mi&src=typd
-
 import tweepy
 import os
-import geopy
-from geopy.geocoders import Nominatim
+import csv
 
+def zip2LatLng(zipcode):
+    with open('zipcodes.csv', 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for i, row in enumerate(spamreader):
+            if i==0:
+                continue
+            zip = row[0].strip('"')
+            if zip == zipcode:
+                return (float(row[5]), float(row[6]))
+    return (None, None)
+
+def hashtags2Query(hashtags):
+    q = ''
+    for i, hashtag in enumerate(hashtags):
+        if i < len(hashtags) - 1:
+            q += (hashtag + ' OR ')
+        else:
+            q += hashtag
+    return q
 
 auth = tweepy.OAuthHandler(os.environ.get('TWITTER_API_KEY'), os.environ.get('TWITTER_API_SECRET'))
 auth.set_access_token(os.environ.get('TWITTER_API_ACCESS_TOKEN'), os.environ.get('TWITTER_API_ACCESS_TOKEN_SECRET'))
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
-q = '#Cardio OR #Cycling OR #Elliptical OR #FitFam OR #FitLife OR #Fitness OR #FitnessAddict OR #GetOutside OR #GetStrong OR #GirlsWhoLift OR #GymLife OR #GymTime OR #NoPainNoGain OR #PersonalTrainer OR #Sweat OR #Treadmill OR #Weights OR #WeightTraining OR #Workout'
+hashtags = ['#Cardio', '#Cycling', '#FitFam', '#FitLife', '#Fitness', '#FitnessAddict', '#Sweat', '#Weights', '#WeightTraining', '#Workout']
 
-geolocator = Nominatim()
-location = geolocator.geocode("30301")
-geocode_str = str(location.latitude) + ',' + str(location.longitude) + ',15mi'
-#37.4262942886,-122.157360145,15mi for zip code 94305
+q = hashtags2Query(hashtags)
+
+lat, lng = zip2LatLng('10002')
+
+geocode_str = str(lat) + ',' + str(lng) + ',10mi'
+
+#30301
+#48205
+#10012
+#10025
+#10002
 
 print(geocode_str)
 
-results = api.search(q, since_id=1, geocode=geocode_str)
+#results = api.search(q, geocode=geocode_str, rpp=100)
 
-print(len(results))
+results = tweepy.Cursor(api.search, q=q, count=100, geocode=geocode_str).items(1000)
 
+count = 0
 for tweet in results:
-  print tweet
-
-#for tweet in public_tweets:
-#     print tweet.text
+  for hashtag in hashtags:
+      if hashtag.lower() in tweet.text.lower():
+          count += 1
+          print(count)
+          print(tweet.text)
+          print(" ")
+          break
